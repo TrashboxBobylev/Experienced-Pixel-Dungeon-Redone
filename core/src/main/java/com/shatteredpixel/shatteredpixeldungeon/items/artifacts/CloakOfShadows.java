50551773ad4a7330d04722bd26462991af1e41a7
TrashboxBobylev <trashbox.bobylev@gmail.com>
2020-07-15 00:41:02 +0700

    2.0: update copyright

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/artifacts/CloakOfShadows.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/artifacts/CloakOfShadows.java
@@ -5,6 +5,9 @@
  * Shattered Pixel Dungeon
  * Copyright (C) 2014-2019 Evan Debenham
  *
+ * Experienced Pixel Dungeon
+ * Copyright (C) 2019-2020 Trashbox Bobylev
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
