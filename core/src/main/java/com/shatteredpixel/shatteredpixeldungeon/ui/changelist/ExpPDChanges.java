3e4799bda056004a9e8613d3aeed7a974e008f6e
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-10-14 20:56:21 +0300

    2.9.5: finale

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/ExpPDChanges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/ExpPDChanges.java
@@ -55,7 +55,19 @@ public class ExpPDChanges {
 
 	public static void addAllChanges( ArrayList<ChangeInfo> changeInfos ){
 
-            ChangeInfo changes = new ChangeInfo("ExpPD-2.9.4", true, "");
+        ChangeInfo changes = new ChangeInfo("ExpPD-2.9.5", true, "");
+        changes.hardlight(Window.TITLE_COLOR);
+        changeInfos.add(changes);
+        changes.addButton( new ChangeButton(Icons.get(Icons.BOBBY_IS_VERY_STRANGE_PERSON_BECAUSE_HE_TRIES_TO_REFERENCE_HIMSELF_IN_NEW_SHATTERED_CREDITS_SCREEN), "Developer Commentary",
+                "_-_ Released October 14th, 2021\n" +
+                        "_-_ 40 days after Experienced Pixel Dungeon 2.9.4\n" +
+                        "\n" +
+                        "Dev commentary will be added here in the future."));
+        changes.addButton( new ChangeButton(Icons.get(Icons.PREFS), "Changes",
+                "_-_ Fixed certain issues with infinity turns.\n\n" +
+                        "_-_ Fishing no longer uses luck."));
+
+            changes = new ChangeInfo("ExpPD-2.9.4", true, "");
             changes.hardlight(Window.TITLE_COLOR);
             changeInfos.add(changes);
             changes.addButton( new ChangeButton(Icons.get(Icons.BOBBY_IS_VERY_STRANGE_PERSON_BECAUSE_HE_TRIES_TO_REFERENCE_HIMSELF_IN_NEW_SHATTERED_CREDITS_SCREEN), "Developer Commentary",
