4cc0d6172ca8d0399e260a840f4b4e686d873c31
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-10-29 03:15:51 +0300

    2.10.1: finale

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/ExpPDChanges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/ExpPDChanges.java
@@ -56,8 +56,17 @@ import java.util.ArrayList;
 public class ExpPDChanges {
 
 	public static void addAllChanges( ArrayList<ChangeInfo> changeInfos ){
-
-        ChangeInfo changes = new ChangeInfo("ExpPD-2.10", true, "");
+        ChangeInfo changes = new ChangeInfo("ExpPD-2.10.1", true, "");
+        changes.hardlight(Window.TITLE_COLOR);
+        changeInfos.add(changes);
+        changes.addButton( new ChangeButton(Icons.get(Icons.BOBBY_IS_VERY_STRANGE_PERSON_BECAUSE_HE_TRIES_TO_REFERENCE_HIMSELF_IN_NEW_SHATTERED_CREDITS_SCREEN), "Developer Commentary",
+                "_-_ Released October 28th, 2021\n" +
+                        "_-_ 1 day after Experienced Pixel Dungeon 2.10\n" +
+                        "\n" +
+                        "Dev commentary will be added here in the future."));
+        changes.addButton( new ChangeButton(Icons.get(Icons.PREFS), "Changes",
+                "_-_ Fixed the crash for rat king beating Tengu."));
+        changes = new ChangeInfo("ExpPD-2.10", true, "");
         changes.hardlight(Window.TITLE_COLOR);
         changeInfos.add(changes);
         changes.addButton( new ChangeButton(Icons.get(Icons.BOBBY_IS_VERY_STRANGE_PERSON_BECAUSE_HE_TRIES_TO_REFERENCE_HIMSELF_IN_NEW_SHATTERED_CREDITS_SCREEN), "Developer Commentary",
