8f68f741246715c19491cb3fec0067f7e183c1cc
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-09-12 22:28:10 +0300

    2.11: removed exit from black mimic level

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/BlackMimicLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/BlackMimicLevel.java
@@ -141,9 +141,6 @@ public class BlackMimicLevel extends Level {
 		Painter.fill(this, 15, 0, 3, 3, Terrain.EXIT);
 
 		int exitCell = 16 + 2*width();
-		LevelTransition exit = new LevelTransition(this, exitCell, LevelTransition.Type.REGULAR_EXIT);
-		exit.set(14, 0, 18, 2);
-		transitions.add(exit);
 
 		CustomTilemap customVisuals = new CityEntrance();
 		customVisuals.setRect(0, 0, width(), 11);
@@ -164,13 +161,6 @@ public class BlackMimicLevel extends Level {
 	@Override
 	public void restoreFromBundle(Bundle bundle) {
 		super.restoreFromBundle(bundle);
-
-		//pre-1.3.0 saves, modifies exit transition with custom size
-		if (bundle.contains("exit")){
-			LevelTransition exit = getTransition(LevelTransition.Type.REGULAR_EXIT);
-			exit.set(14, 0, 18, 2);
-			transitions.add(exit);
-		}
 
 		for (CustomTilemap c : customTiles){
 			if (c instanceof ArenaVisuals){
