8915996033f8c154249166e21601d1adb533dc4f
Zachary Perlmutter <zrp200@gmail.com>
2022-09-14 14:05:10 -0700

    created file for variable handling

---
+++ Variable.java
@@ -0,0 +1,95 @@
+package com.zrp200.scrollofdebug;
+
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
+import com.watabou.utils.Bundlable;
+import com.watabou.utils.Bundle;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+
+abstract public class Variable<T> implements Bundlable {
+    private static HashMap<String, Variable> all = new HashMap();
+
+    static boolean put(String key, Object o) {
+        if (key == null) return true; // no variable to store.
+        if (o instanceof Actor) {
+            all.put(key, new ActorVariable((Actor) o));
+            return true;
+        }
+        return false;
+    }
+
+    public static HashMap<String, Variable> getActive() {
+        HashMap<String, Variable> active = new HashMap(all);
+        {
+            Iterator<Map.Entry<String, Variable>> entries = all.entrySet().iterator();
+            Map.Entry<?, Variable> entry;
+            while (entries.hasNext()) {
+                entry = entries.next();
+                if (!entry.getValue().isActive()) {
+                    entries.remove();
+                }
+            }
+        }
+        return active;
+    }
+
+    abstract T getTarget();
+
+    boolean isActive() {
+        return getTarget() != null;
+    }
+
+    @Override
+    public int hashCode() {
+        T target = getTarget();
+        return target != null ? target.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj != null && obj.getClass() == getClass() && hashCode() == obj.hashCode();
+    }
+
+    @Override
+    public void storeInBundle(Bundle bundle) {}
+
+    @Override
+    public void restoreFromBundle(Bundle bundle) {}
+
+    public static class ActorVariable extends Variable<Actor> {
+        private int id, depth;
+
+        @Deprecated
+        ActorVariable() {} // for Bundling only
+
+        ActorVariable(Actor actor) {
+            id = actor.id();
+            depth = Dungeon.depth;
+        }
+
+        @Override
+        Actor getTarget() { return Actor.findById(id); }
+
+        @Override
+        public String toString() {
+            Actor target = getTarget();
+            if (target == null) return null;
+            return target.getClass().getSimpleName() + " (" + id + ")";
+        }
+
+        @Override
+        public void storeInBundle(Bundle bundle) {
+            bundle.put("id", id);
+            bundle.put("depth", depth);
+        }
+
+        @Override
+        public void restoreFromBundle(Bundle bundle) {
+            id = bundle.getInt("id");
+            depth = bundle.getInt("depth");
+        }
+    }
+}
