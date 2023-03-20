8bfd15f29ed5b580840cbcda0828d957434338cc
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2023-03-21 01:54:53 +0300

    2.14: black mimic's level is considered a boss level

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -433,7 +433,7 @@ public class Dungeon {
 	}
 	
 	public static boolean bossLevel( int depth ) {
-		return depth == 5 || depth == 10 || depth == 15 || depth == 20 || depth == 25;
+		return depth == 5 || depth == 10 || depth == 15 || depth == 20 || depth == 25 || depth == 27;
 	}
 
 	//value used for scaling of damage values and other effects.
