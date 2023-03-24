1f5c855c6d19b81a75e364b9ae35c769bb99038b
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2023-03-24 21:53:20 +0300

    2.14: added internalTier to tengu's shuriken

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/TenguShuriken.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/TenguShuriken.java
@@ -38,7 +38,7 @@ public class TenguShuriken extends MissileWeapon {
         hitSound = Assets.Sounds.HIT_STAB;
         hitSoundPitch = 1.4f;
 
-        tier = 2;
+        internalTier = tier = 2;
         baseUses = 25;
     }
 
