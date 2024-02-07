5a139fca47c8ae2c32b4fa86351504409ba044ae
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-02-08 01:41:38 +0700

    2.17: reimplemented crash reports (never commit main code on GP branch, never!)

--- android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
+++ android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
@@ -39,6 +39,7 @@ import com.badlogic.gdx.backends.android.AndroidAudio;
 import com.badlogic.gdx.backends.android.AsynchronousAndroidAudio;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeType;
 import com.badlogic.gdx.utils.GdxNativesLoader;
+import com.rohitss.uceh.UCEHandler;
 import com.shatteredpixel.shatteredpixeldungeon.SPDSettings;
 import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
 import com.shatteredpixel.shatteredpixeldungeon.services.news.News;
@@ -71,7 +72,7 @@ public class AndroidLauncher extends AndroidApplication {
 			return;
 		}
 
-//		new UCEHandler.Builder(this).setUCEHEnabled(true).build();
+		new UCEHandler.Builder(this).setUCEHEnabled(true).build();
 
 		//there are some things we only need to set up on first launch
 		if (instance == null) {
