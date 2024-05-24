72769a463877ce488dd0184c7683660e15f932d7
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2024-05-25 00:36:03 +0700

    2.18: restored experienced's duelist subclasses perks

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
@@ -305,6 +305,7 @@ public class Hero extends Char {
 		if (talent == Talent.ENRAGED_CATALYST && subClass == HeroSubClass.BERSERKER) return 4;
 		if (talent == Talent.EMPOWERED_STRIKE && subClass == HeroSubClass.BATTLEMAGE) return 6;
 		if (talent == Talent.SPEEDY_STEALTH && subClass == HeroSubClass.FREERUNNER) return 3;
+		if (talent == Talent.VARIED_CHARGE && subClass == HeroSubClass.CHAMPION) return 6;
 		for (LinkedHashMap<Talent, Integer> tier : talents){
 			for (Talent f : tier.keySet()){
 				if (f == talent) return tier.get(f);
